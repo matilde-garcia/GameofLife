16ae65184b6962a55217c7cd59647fd657df45f9
matilde-garcia <m.garcia5@campus.unimib.it>
2025-11-27 16:54:16 +0100

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='gameoflife_matilde_module',
       description='test module for the SciComp class',
-      url='https:///github.com/matilde-garcia',
+      url='https://github.com/matilde-garcia/GameofLife',
       author='Matilde Garcia',
       author_email='m.garcia5@campus.unimib.it',
       license='MIT',
