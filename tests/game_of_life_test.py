ee649e836da71c7faab7e7e6ebfcc319f2ebe2e2
matilde-garcia <m.garcia5@campus.unimib.it>
2026-07-24 15:24:39 +0200

    update game_of_life_functions and tests

--- tests/game_of_life_test.py
+++ tests/game_of_life_test.py
@@ -1,15 +1,19 @@
 import pytest
 import numpy as np
+import os
 import gameoflife_matilde_module
 
 
-"""We want to implement a unit test and a regression test. Therefore, we will test if 
+"""
+We want to implement a unit test and a regression test. Therefore, we will test if 
 some function is doing what it is supposed to be doing (unit testing). Then, we will 
-also test if some change in the code changed the way the code should work."""
+also test if some change in the code changed the way the code should work.
+"""
 
 #-------------------------------------------------------UNIT TESING-------------------------------------------------------
 
-my_grid = np.random.randint(low = 0, high=2, size = (50,50))
+generator = np.random.default_rng(0)
+my_grid = (generator.random((50, 50)) < 0.30).astype(int)
 
 #pretty lame test, but let's make sure it returns a grid with the same size
 def test_lifegame():
@@ -22,12 +26,13 @@ def test_lifegame2():
 #------------------------------------------------------REGRESSION TESTING----------------------------------------------------
 
 #grid generated for the benchmark results: for accurate testing this should NEVER be changed!!!!
-np.random.seed(0)
-my_grid = np.random.randint(low = 0, high = 2, size = (20,20))
+initial_path = os.path.join(os.path.dirname(__file__), "benchmark", "initial_grid.txt")
+my_grid = np.loadtxt(initial_path, dtype=int, delimiter=' ')
 
 #test that upcoming executions of the code will lead to the same result for a starting grid of 20x20, after 10 iterations
 def test_lifegame3():
     my_ani = gameoflife_matilde_module.play_life_game_wplots(my_grid, 10)
-    benchmark_final_grid = np.loadtxt("benchmark/final_grid.txt", dtype=int, delimiter=' ')
+    benchmark_path = os.path.join(os.path.dirname(__file__), "benchmark", "final_grid.txt")
+    benchmark_final_grid = np.loadtxt(benchmark_path, dtype=int, delimiter=' ')
     this_final_grid = gameoflife_matilde_module.play_life_game(my_grid, 10)[-1]
     assert np.array_equal(this_final_grid, benchmark_final_grid)
